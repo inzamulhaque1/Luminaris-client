cbf3678e5c2be5d2531fe3a3a2d9e2262e5a798e
MD INZAMUL HAQUE JOY <inzamulhaque1002@gmail.com>
2025-04-26 23:52:33 +0600

    added co,or

--- src/pages/Auth/Login.jsx
+++ src/pages/Auth/Login.jsx
@@ -74,7 +74,7 @@ const Login = () => {
             
             {/* Error message */}
             {loginError && (
-              <div className="mb-6 p-3 bg-red-50 border-l-4 border-red-500 text-blue-700 rounded">
+              <div className="mb-6 p-3 bg-red-50 border-l-4 border-red-500 text-orange-700 rounded">
                 <p className="text-sm flex items-center">
                   <svg xmlns="http://www.w3.org/2000/svg" className="h-5 w-5 mr-2" viewBox="0 0 20 20" fill="currentColor">
                     <path fillRule="evenodd" d="M18 10a8 8 0 11-16 0 8 8 0 0116 0zm-7 4a1 1 0 11-2 0 1 1 0 012 0zm-1-9a1 1 0 00-1 1v4a1 1 0 102 0V6a1 1 0 00-1-1z" clipRule="evenodd" />
