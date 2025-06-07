ea09006ad02af31aee013092c096d932ed9fe52d
MD INZAMUL HAQUE JOY <inzamulhaque1002@gmail.com>
2025-06-07 18:10:55 +0600

    added sweetalert

--- src/routes/routes.jsx
+++ src/routes/routes.jsx
@@ -44,10 +44,7 @@ import AllUsers from "../pages/Dashboard/Admin/AllUsers";
           path: 'all-users',
           element: <AllUsers></AllUsers>
         },
-        {
-          path: 'all-users',
-          element: <AllUsers></AllUsers>
-        },
+        
       ]
     }
   ]);
